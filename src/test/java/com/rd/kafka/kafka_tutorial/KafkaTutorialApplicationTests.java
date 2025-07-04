949e50c092820d0cb40c6d3628b8e529e75fb529
rishu <rishudixit@gmail.com>
2025-07-04 05:31:33 +0530

    Kafka Project first commit

---
+++ src/test/java/com/rd/kafka/kafka_tutorial/KafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rd.kafka.kafka_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
